5344bcc8be581bd69432ba46a7dd39a1f49640a8
Alexandra Peccia <ampeccia@gmail.com>
2021-01-23 20:58:33 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -5,7 +5,7 @@ def run_guessing_game
   
   if user_input == 'exit'
     puts "Goodbye!"
-  elsif user_input == computer_number 
+  elsif user_input.to_i == computer_number 
     puts "You guessed the correct number!"
   elsif user_input != computer_number
     puts "Sorry! The computer guessed #{computer_number}."
