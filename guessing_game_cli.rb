cbecd1a31b916f5a70731fea65c790784a87ca9e
Alexandra Peccia <ampeccia@gmail.com>
2021-01-23 21:09:04 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -6,13 +6,13 @@ def run_guessing_game
   
   user_input = gets.chomp 
   
-  if user_input == 'exit'
-    puts "Goodbye!"
-  elsif user_input.to_i == computer_number 
-    puts "You guessed the correct number!"
-  elsif user_input.to_i != computer_number || user_input != 'exit'
-    puts "Sorry! The computer guessed #{computer_number}."
-  end
+  # if user_input == 'exit'
+  #   puts "Goodbye!"
+  # elsif user_input.to_i == computer_number 
+  #   puts "You guessed the correct number!"
+  # elsif user_input.to_i != computer_number || user_input != 'exit'
+  #   puts "Sorry! The computer guessed #{computer_number}."
+  # end
 end 
 
 
